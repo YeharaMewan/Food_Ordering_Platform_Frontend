7ab76e0b753e649c6acff945045a19d0456c11a4
YeharaMewan <125129373+YeharaMewan@users.noreply.github.com>
2025-09-11 11:29:48 +0530

    remove valnarability issues

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -1,23 +1,29 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useGetMyUser } from "@/api/MyUserApi";
 
 const MobileNavLinks = () => {
   const { logout } = useAuth0();
+  const { currentUser } = useGetMyUser();
+  
   return (
     <>
-      <Link
-        to="/order-status"
-        className="flex bg-white items-center font-bold hover:text-orange-500"
-      >
-        Order Status
-      </Link>
-      <Link
-        to="/manage-restaurant"
-        className="flex bg-white items-center font-bold hover:text-orange-500"
-      >
-        My Restaurant
-      </Link>
+      {currentUser?.role === "admin" ? (
+        <Link
+          to="/manage-restaurant"
+          className="flex bg-white items-center font-bold hover:text-orange-500"
+        >
+          My Restaurant
+        </Link>
+      ) : (
+        <Link
+          to="/order-status"
+          className="flex bg-white items-center font-bold hover:text-orange-500"
+        >
+          Order Status
+        </Link>
+      )}
       <Link
         to="/user-profile"
         className="flex bg-white items-center font-bold hover:text-orange-500"
