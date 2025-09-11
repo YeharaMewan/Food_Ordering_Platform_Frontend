3605264fad621c5fb19779ba4795f3d8c99d15c3
YeharaMewan <125129373+YeharaMewan@users.noreply.github.com>
2025-09-11 11:55:52 +0530

    remove build error of UsernameMenu.tsx

--- src/components/UsernameMenu.tsx
+++ src/components/UsernameMenu.tsx
@@ -19,7 +19,7 @@ const UsernameMenu = () => {
     <DropdownMenu>
       <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-orange-500 gap-2">
         <CircleUserRound className="text-orange-500" />
-        {user?.email}
+        {currentUser?.name || user?.email}
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem>
